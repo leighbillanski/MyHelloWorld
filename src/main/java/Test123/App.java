daf6a387600a31438473f9fe591ebdbf7ce5e0b7
Tcheslavie <valeryburtsev@yahoo.com>
2019-02-13 09:18:03 +0200

    Add files via upload

---
+++ src/main/java/Test123/App.java
@@ -0,0 +1,15 @@
+package Test123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        NewClass test = new NewClass();
+        test.sayHello();
+    }
+}
---
+++ src/main/java/Test123/NewClass.java
@@ -0,0 +1,9 @@
+package Test123;
+
+public class NewClass {
+    public NewClass(){}
+
+    public void sayHello(){
+        System.out.println("Hey there");
+    }
+}
